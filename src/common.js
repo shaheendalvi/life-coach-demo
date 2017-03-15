dc7fbb4d0d650e864bc650ae9eb4c0c6deeb80e6
Gagandeep Kaur <gagan.cheema@gmail.com>
2017-03-15 14:27:16 +1100

    icons

---
+++ src/common.js
@@ -0,0 +1,10 @@
+var colors = {
+    "FF" : "#BD3BBF", //Family & Friends
+	"HL": "#346951",//Health
+	"FN": "#767676", //Finance
+	"CR": "#60A6DA", //Career
+	"PG": "#FF6200", //Personal Growth
+	"HM": "#3D3DAA", //Home
+	"RM": "#AB0066", //Romance
+	"FH": "#D0D93C" //Fun & Hobbies
+}
